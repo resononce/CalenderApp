e4a37cf4887ace9288215b6c6dd0ef67ea93d16b
Esteban Lopez <lopeze37@uwosh.edu>
2019-10-08 08:26:03 -0500

    init

---
+++ SoftEng/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftEng.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
