fd6d6f9f9875c33d498167bc78ac102e61f77bc3
Otto, Jonathan <ottoj11@uwosh.edu>
2019-10-20 18:37:55 -0500

    Added additional field for registering

---
+++ SoftEng/wwwroot/js/Register.js
@@ -0,0 +1,12 @@
+﻿
+
+function toggleRegisterLogin() {
+    var checked = $("#registerCheckbox").is(":checked");
+    if (checked) {
+        $("#userLoginBtn").removeClass("hidden").addClass("hidden");
+        $("#registerFields").removeClass("hidden");
+    } else {
+        $("#registerFields").removeClass("hidden").addClass("hidden");
+        $("#userLoginBtn").removeClass("hidden");
+    }
+}
