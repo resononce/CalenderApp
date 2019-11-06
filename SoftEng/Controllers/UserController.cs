89ed9a6a295bbb5fc5278e35efb83ce75cd4a343
Otto, Jonathan <ottoj11@uwosh.edu>
2019-11-05 23:10:53 -0600

    User home page displays events.  (currently not hooked up to db; just returns hard coded event)

--- SoftEng/Controllers/UserController.cs
+++ SoftEng/Controllers/UserController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SoftEng.Models;
+using SoftEng.DataAccess;
+using SoftEng.DataAccess.DataObjects;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,20 @@ namespace SoftEng.Controllers
 {
     public class UserController : Controller
     {
+        private readonly Database db;
+
+        public UserController(team7Context context)
+        {
+            this.db = new Database(context);
+        }
         // GET: /<controller>/
         public IActionResult Home()
         {
-            return View();
+            UserHomeModel model = new UserHomeModel();
+            model.UserHomeEvents = new List<Event>();
+            model.UserHomeEvents.Add(new Event { Id = 1, EventDate = new DateTime(), EventTime = new TimeSpan(), Name = "Test", Location = "Test Place" });
+            //model.UserHomeEvents = this.db.GetUserEvents();
+            return View(model);
         }
 
         public IActionResult WeeklyView()
---
+++ SoftEng/Models/UserHomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SoftEng.DataAccess.DataObjects;
+
+namespace SoftEng.Models
+{
+    public class UserHomeModel
+    {
+        public List<Event> UserHomeEvents { get; set; }
+    }
+}
