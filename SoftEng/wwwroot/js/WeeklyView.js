e3cb4b1ac676f7df6c0beae94b408430e463e2f9
Otto, Jonathan <ottoj11@uwosh.edu>
2019-12-04 14:02:00 -0600

    Added a date display for Weekly View

--- SoftEng/wwwroot/js/WeeklyView.js
+++ SoftEng/wwwroot/js/WeeklyView.js
@@ -1,4 +1,30 @@
-﻿
+﻿var startDate;
+var endDate;
+window.onload = function () {
+    startDate = new Date();
+    endDate = new Date();
+    var day = startDate.getDay() * -1;
+    startDate.setTime(startDate.getTime() + day * 86400000);
+    endDate.setTime(startDate.getTime() + 6 * 86400000);
+    $("#startDate").html(startDate.getMonth() + 1 + "/" + startDate.getDate() + "/" + startDate.getFullYear());
+    $("#endDate").html(endDate.getMonth() + 1 + "/" + endDate.getDate() + "/" + endDate.getFullYear());
+}
+
+function back() {
+    startDate.setTime(startDate.getTime() + -7 * 86400000);
+    endDate.setTime(endDate.getTime() + -7 * 86400000);
+    $("#startDate").html(startDate.getMonth() + 1 + "/" + startDate.getDate() + "/" + startDate.getFullYear());
+    $("#endDate").html(endDate.getMonth() + 1 + "/" + endDate.getDate() + "/" + endDate.getFullYear());
+}
+
+function forward() {
+    debugger;
+    startDate.setTime(startDate.getTime() + 7 * 86400000);
+    endDate.setTime(endDate.getTime() + 7 * 86400000);
+    $("#startDate").html(startDate.getMonth() + 1 + "/" + startDate.getDate() + "/" + startDate.getFullYear());
+    $("#endDate").html(endDate.getMonth() + 1 + "/" + endDate.getDate() + "/" + endDate.getFullYear());
+}
+
 function requestWeek() {
     var startDate = new Date();
     var endDate = new Date();
