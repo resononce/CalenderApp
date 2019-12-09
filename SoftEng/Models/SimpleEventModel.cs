e874a17373068897fba795ed16b4ab9b1bc78488
Esteban Lopez <lopeze37@uwosh.edu>
2019-12-09 11:22:54 -0600

    forgot model lmao

---
+++ SoftEng/Models/SimpleEventModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftEng.Models
+{
+    public class SimpleEventModel
+    {
+        public string name { get; set; }
+        public int day { get; set; }
+        public int time { get; set; }
+        public int timeEnd { get; set; }
+    }
+}
