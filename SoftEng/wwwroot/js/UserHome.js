490d429cf18e79b20a0f2e27d34d6855fcf5ba29
Cheng <uncreatedcreator@hotmail.com>
2019-12-11 21:11:05 -0600

    User homepage looks a little prettier

---
+++ SoftEng/wwwroot/js/UserHome.js
@@ -0,0 +1,7 @@
+﻿function progress() {
+        var valeur = 0;
+        var numberOfChecked = $('input:checkbox:checked').length;
+        var totalCheckboxes = $('input:checkbox').length;
+        var percent = (numberOfChecked / totalCheckboxes).toFixed(2) * 100;
+        $('.progress-bar').css('width', percent + '%').attr('aria-valuenow', percent);
+}
